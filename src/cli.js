0643a5a6efeb518ecb8da30f90f765cb093bbc82
aleksei-shvets <allinfome@yandex.ru>
2023-05-09 20:57:51 +0300

    import readline-sync and create 'greeting' func

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import * as readlineSync from 'readline-sync';
+
+const greeting = () => {
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`)
+};
+
+export { greeting };
