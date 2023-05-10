dd4fbdbe47bdf1ba150982d10759b6ded904671b
aleksei-shvets <allinfome@yandex.ru>
2023-05-10 21:43:50 +0300

    fix in braingames.ls and cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import * as readlineSync from 'readline-sync';
 
 const greeting = () => {
   const userName = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${userName}!`)
+  console.log(`Hello, ${userName}!`);
 };
 
-export { greeting };
+export default greeting;
