c0652d9314778b820b4c5a53b09b06870bd3bcb1
aleksei-shvets <allinfome@yandex.ru>
2023-05-26 12:00:05 +0300

    change function for operatorsList to randomArreyIndex

--- games/calc.js
+++ games/calc.js
@@ -1,4 +1,6 @@
-import { inputOut, randomInt, greetingInStart } from '../src/index.js';
+import {
+  inputOut, randomInt, greetingInStart, randomArreyIndex,
+} from '../src/index.js';
 
 const userName = greetingInStart();
 console.log('What is the result of the expression?');
@@ -8,7 +10,7 @@ const calc = () => {
     const firstNumber = randomInt(10);
     const secondNumber = randomInt(10);
     const operatorsList = ['+', '-', '*'];
-    const indexOfOperatorList = randomInt(operatorsList.length);
+    const indexOfOperatorList = randomArreyIndex(operatorsList);
     const operator = operatorsList[indexOfOperatorList];
     let expression;
     switch (operator) {
