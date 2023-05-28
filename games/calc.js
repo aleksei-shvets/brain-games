65bd0e9810f27089b67e88cb80d8ccabd2391f52
aleksei-shvets <allinfome@yandex.ru>
2023-05-28 15:54:22 +0300

    refactoring calc.js to new logic in code

--- games/calc.js
+++ games/calc.js
@@ -1,42 +1,40 @@
 import {
-  inputOut, randomInt, greetingInStart, randomArreyIndex,
+  interactivOfGame, randomInt, greetingInStart, randomArreyIndex,
 } from '../src/index.js';
 
-const userName = greetingInStart();
-console.log('What is the result of the expression?');
+const logic = () => {
+  const firstNumber = randomInt(10);
+  const secondNumber = randomInt(10);
+  const operatorsList = ['+', '-', '*'];
+  const indexOfOperatorList = randomArreyIndex(operatorsList);
+  const operator = operatorsList[indexOfOperatorList];
+  let correctAnswer;
+  switch (operator) {
+    case '+':
+      correctAnswer = firstNumber + secondNumber;
+      break;
+    case '-':
+      correctAnswer = firstNumber - secondNumber;
+      break;
+    case '*':
+      correctAnswer = firstNumber * secondNumber;
+      break;
+    default:
+      break;
+  }
+  const questionLine = `${firstNumber} ${operator} ${secondNumber}`;
+
+  const output = [];
+  output[0] = questionLine;
+  output[1] = correctAnswer;
+  return output;
+};
 
 const calc = () => {
-  for (let count = 1; count <= 3; count += 1) {
-    const firstNumber = randomInt(10);
-    const secondNumber = randomInt(10);
-    const operatorsList = ['+', '-', '*'];
-    const indexOfOperatorList = randomArreyIndex(operatorsList);
-    const operator = operatorsList[indexOfOperatorList];
-    let expression;
-    switch (operator) {
-      case '+':
-        expression = firstNumber + secondNumber;
-        break;
-      case '-':
-        expression = firstNumber - secondNumber;
-        break;
-      case '*':
-        expression = firstNumber * secondNumber;
-        break;
-      default:
-        break;
-    }
-    const questionLine = `${firstNumber} ${operator} ${secondNumber}`;
-    const answer = inputOut(`Question: ${questionLine} \nYour answer: `);
-    if (answer === String(expression)) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${expression}.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+  const userName = greetingInStart();
+  console.log('What is the result of the expression?');
+
+  interactivOfGame(logic, userName);
 };
 
 export default calc;
