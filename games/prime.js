981b0c0729775ee0a94d812fa51c370a11d66d97
aleksei-shvets <allinfome@yandex.ru>
2023-05-28 15:56:01 +0300

    refactoring prime.js to new logic in code

--- games/prime.js
+++ games/prime.js
@@ -1,37 +1,36 @@
-import { inputOut, randomInt, greetingInStart } from '../src/index.js';
+import { interactivOfGame, randomInt, greetingInStart } from '../src/index.js';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return 'no';
+  }
+  let count = 0;
+  for (let i = 1; i <= Math.round(num / 2); i += 1) {
+    if (num % i === 0) {
+      count += 1;
+    }
+  }
+  if (count > 1) {
+    return 'no';
+  }
+  return 'yes';
+};
+
+const logic = () => {
+  const questionLine = randomInt(20);
+  const correctAnswer = isPrime(questionLine);
+
+  const output = [];
+  output[0] = questionLine;
+  output[1] = correctAnswer;
+  return output;
+};
 
 const prime = () => {
   const userName = greetingInStart();
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  const isPrime = (num) => {
-    if (num < 2) {
-      return 'no';
-    }
-    let count = 0;
-    for (let i = 1; i <= Math.round(num / 2); i += 1) {
-      if (num % i === 0) {
-        count += 1;
-      }
-    }
-    if (count > 1) {
-      return 'no';
-    }
-    return 'yes';
-  };
 
-  for (let count = 1; count <= 3; count += 1) {
-    const number = randomInt(20);
-    const correctAnswer = isPrime(number);
-    const answer = inputOut(`Question: ${number} \nYour answer: `);
-    if (answer === String(correctAnswer)) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+  interactivOfGame(logic, userName);
 };
 
 export default prime;
