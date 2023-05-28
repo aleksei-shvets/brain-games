57878bd36d4fc81019b253a7db61297deb6eaccb
aleksei-shvets <allinfome@yandex.ru>
2023-05-28 15:51:11 +0300

    add interactivOfGame function

--- src/index.js
+++ src/index.js
@@ -16,6 +16,24 @@ const greetingInStart = () => {
   return user;
 };
 
+const interactivOfGame = (logicGenerator, userName) => {
+  for (let count = 1; count <= 3; count += 1) {
+    const logic = logicGenerator();
+    const questionLine = logic[0];
+    const correctAnswer = logic[1];
+    const answer = inputOut(`Question: ${questionLine} \nYour answer: `);
+    if (answer === String(correctAnswer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
 export {
   inputOut, randomInt, greetingInStart, randomArreyIndex,
+  interactivOfGame,
 };
