bdbb0b11f71a91d9606460e1472e29a4ae6295ed
aleksei-shvets <allinfome@yandex.ru>
2023-05-28 15:55:29 +0300

    refactoring even.js to new logic in code

--- games/even.js
+++ games/even.js
@@ -1,27 +1,23 @@
-import { inputOut, randomInt, greetingInStart } from '../src/index.js';
+import { interactivOfGame, randomInt, greetingInStart } from '../src/index.js';
 
-const userName = greetingInStart();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const logic = () => {
+  const questionLine = randomInt(100);
+  let correctAnswer;
+  if (questionLine % 2 === 0) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+  const output = [];
+  output[0] = questionLine;
+  output[1] = correctAnswer;
+  return output;
+};
 
 const even = () => {
-  for (let count = 1; count <= 3; count += 1) {
-    const question = randomInt(100);
-    const answer = inputOut(`Question: ${question}\nYour answer: `);
-    let correctAnswer;
-    if (question % 2 === 0) {
-      correctAnswer = 'yes';
-    } else {
-      correctAnswer = 'no';
-    }
-
-    if (answer !== correctAnswer) {
-      console.log(`${answer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${userName}!`);
+  const userName = greetingInStart();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  interactivOfGame(logic, userName);
 };
 
 export default even;
