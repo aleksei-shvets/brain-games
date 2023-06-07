8f781301b7c0300741a7ae174ce169efec1b9df7
aleksei-shvets <allinfome@yandex.ru>
2023-06-07 20:03:21 +0300

    refuctoring isEven and logic functions

--- src/games/even.js
+++ src/games/even.js
@@ -1,19 +1,11 @@
 import startOfGame from '../index.js';
 import randomInt from '../utils.js';
 
-const isEven = (num) => {
-  let answer;
-  if (num % 2 === 0) {
-    answer = 'yes';
-  } else {
-    answer = 'no';
-  }
-  return answer;
-};
+const isEven = (num) => num % 2 === 0;
 
 const logic = () => {
   const questionLine = randomInt(1, 100);
-  const correctAnswer = isEven(questionLine);
+  const correctAnswer = isEven(questionLine) ? 'yes' : 'no';
   const output = [];
   output[0] = questionLine;
   output[1] = correctAnswer;
