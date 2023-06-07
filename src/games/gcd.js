bea96530df4559e638a9991fb9a40646be8be9c6
aleksei-shvets <allinfome@yandex.ru>
2023-06-07 20:25:58 +0300

    refuctoring generationCorrectAnswer in gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,29 +2,24 @@ import startOfGame from '../index.js';
 import randomInt from '../utils.js';
 
 const generationCorrectAnswer = (numOne, numTwo) => {
-  const marker = numOne >= numTwo ? numOne : numTwo;
-  const upperBound = Math.floor(marker / 2);
-  let answer;
-  let gcdNumber;
-
   if (numOne === numTwo) {
-    answer = String(numOne);
-  } else {
-    for (let i = upperBound; gcdNumber === undefined; i -= 1) {
-      if (numOne % i === 0 && numTwo % i === 0) {
-        gcdNumber = i;
-      }
-      answer = String(gcdNumber);
+    return numOne;
+  }
+  while (numOne !== 0 && numTwo !== 0) {
+    if (numOne > numTwo) {
+      numOne %= numTwo;
+    } else {
+      numTwo %= numOne;
     }
   }
-  return answer;
+  return numOne + numTwo;
 };
 
 const logic = () => {
   const firstNumber = randomInt(1, 60);
-  const secondNumber = randomInt(1, 50);
+  const secondNumber = randomInt(1, 60);
 
-  const correctAnswer = generationCorrectAnswer(firstNumber, secondNumber);
+  const correctAnswer = String(generationCorrectAnswer(firstNumber, secondNumber));
 
   const questionLine = `${firstNumber} ${secondNumber}`;
 
@@ -41,3 +36,22 @@ const gcd = () => {
 };
 
 export default gcd;
+
+/* const generationCorrectAnswer = (numOne, numTwo) => {
+  const marker = numOne >= numTwo ? numOne : numTwo;
+  const upperBound = Math.floor(marker / 2);
+  let answer;
+  let gcdNumber;
+
+  if (numOne === numTwo) {
+    answer = String(numOne);
+  } else {
+    for (let i = upperBound; gcdNumber === undefined; i -= 1) {
+      if (numOne % i === 0 && numTwo % i === 0) {
+        gcdNumber = i;
+      }
+      answer = String(gcdNumber);
+    }
+  }
+  return answer;
+}; */
