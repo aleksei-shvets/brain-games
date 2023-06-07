03e3fe75d74cde456f7010d835fa0705884af001
aleksei-shvets <allinfome@yandex.ru>
2023-06-07 18:57:31 +0300

    Correct to independent code in brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
-import { greetingInStart } from '../src/utils.js';
+import * as readlineSync from 'readline-sync';
+
+const inputOut = (question) => {
+  const answer = readlineSync.question(question);
+  return answer;
+};
+
+const greetingInStart = () => {
+  console.log('Welcome to the Brain Games!');
+  const user = inputOut('May I have your name? ');
+  console.log(`Hello, ${user}!`);
+  return user;
+};
 
 greetingInStart();
