6ac07ba4b7d83b78e0be978afca0a79a9c30ee33
aleksei-shvets <allinfome@yandex.ru>
2023-06-07 21:35:10 +0300

    refactoring brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,16 +2,6 @@
 
 import * as readlineSync from 'readline-sync';
 
-const inputOut = (question) => {
-  const answer = readlineSync.question(question);
-  return answer;
-};
-
-const greetingInStart = () => {
-  console.log('Welcome to the Brain Games!');
-  const user = inputOut('May I have your name? ');
-  console.log(`Hello, ${user}!`);
-  return user;
-};
-
-greetingInStart();
+console.log('Welcome to the Brain Games!');
+const user = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${user}!`);
