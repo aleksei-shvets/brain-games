ace3fc4e8822cae4639ab733746aabbf3c78407a
aleksei-shvetz <al.sv.dev@gmail.com>
2023-06-17 13:45:38 +0300

    refactoring progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,20 +3,18 @@ import randomInt from '../utils.js';
 
 const progressionLength = 10;
 
-const progressionStepCreate = () => randomInt(0, 20) - randomInt(0, 20);
-
-const startNumberCreate = () => randomInt(0, 20);
-
-const progressionGeneration = (startNumber, progressionStep) => {
+const generateProgression = (firstNum, step) => {
   const sequence = [];
-  for (let num = startNumber; sequence.length <= progressionLength; num += progressionStep) {
+  for (let num = firstNum; sequence.length <= progressionLength; num += step) {
     sequence.push(num);
   }
   return sequence;
 };
 
 const logicFormation = () => {
-  const gameSequence = progressionGeneration(startNumberCreate(), progressionStepCreate());
+  const progressionStep = randomInt(0, 20) - randomInt(0, 20);
+  const startNumber = randomInt(0, 20);
+  const gameSequence = generateProgression(startNumber, progressionStep);
   const missingNumderIndex = randomInt(0, gameSequence.length);
   const correctAnswer = String(gameSequence[missingNumderIndex]);
   gameSequence[missingNumderIndex] = '..';
